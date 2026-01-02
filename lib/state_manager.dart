33d62e4c8d8f7f4fd833e026c7da836babd29244
Benjamin <benjaminsalazarballadares@gmail.com>
2026-01-02 17:42:14 -0300

    Add support to clean the current state manager

--- lib/state_manager.dart
+++ lib/state_manager.dart
@@ -7,16 +7,23 @@ class StateManager {
 
   StateManager() : states = Map();
 
+  // Checks if a widget with the given id has been added
   bool hasWidget(int id) {
     return states.containsKey(id);
   }
 
+  // Creates a state if it does not already exist
   void createStateIfNotExists(int id, Widget? widget, dynamic value) {
     if(hasWidget(id)) {
       return;
     }
 
     states[id] = Pair(first: widget, second: value);
+  }
+
+  // Clears all states. Only use this function when disposing the page state
+  void clear() {
+    states.clear();
   }
 
   dynamic getStateValue(int id) => states[id]!.second;
